86a1aa057490c8c21e5fca4dc6c1980b9c7c8c7e
Ruslan Ismayilov <ruslanismayilov484@gmail.com>
2021-06-02 19:19:05 -0700

    Model is created

--- Randog/Controller/ViewController.swift
+++ Randog/Controller/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let randomImageEndpoint = DogAPI.Endpoint.randomImageFromAllDogCollection.url
     }
 
 
---
+++ Randog/Model/DogAPI.swift
@@ -0,0 +1,18 @@
+//
+//  DogAPI.swift
+//  Randog
+//
+//  Created by Ruslan Ismayilov on 6/2/21.
+//
+
+import Foundation
+class DogAPI {
+    enum Endpoint : String {
+        case randomImageFromAllDogCollection = "https://dog.ceo/api/breeds/image/random"
+        
+        var url : URL {
+            return URL(string: self.rawValue)!
+        }
+    }
+    
+}
