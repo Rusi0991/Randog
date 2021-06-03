5ff9087f17105fba57f2bae535ed939b24fa5442
Ruslan Ismayilov <ruslanismayilov484@gmail.com>
2021-06-02 18:29:21 -0700

    Randog project is created

---
+++ Randog/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Randog
+//
+//  Created by Ruslan Ismayilov on 6/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
