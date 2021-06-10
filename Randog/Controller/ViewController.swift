9a5486a4e94f8118e5b462ad8e00ae190a59c97e
Ruslan Ismayilov <ruslanismayilov484@gmail.com>
2021-06-09 18:48:33 -0700

    Final project

--- Randog/Controller/ViewController.swift
+++ Randog/Controller/ViewController.swift
@@ -10,40 +10,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let randomImageEndpoint = DogAPI.Endpoint.randomImageFromAllDogCollection.url
+        DogAPI.requestRandomImage(completionHandler:handleRandomImageResponse(imageData:error:))
+       
         
-        let task = URLSession.shared.dataTask(with: randomImageEndpoint) { (data, responce, error) in
-            guard let data = data else{
-                return
-            }
-//            print(data)
-//            do{
-//                let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
-//                let url = json["message"] as! String
-//                print(url)
-//
-//            }catch{
-//                print("error")
-//            }
-            let decoder = JSONDecoder()
-            let imageData = try! decoder.decode(DogImage.self, from: data)
-            print(imageData)
-            guard let imageUrl = URL(string: imageData.message) else{
-                return
-            }
-            let task = URLSession.shared.dataTask(with: imageUrl) { (data, response, error) in
-                guard let data = data else{
-                    return
-                    
-                }
-                let downloadedImage = UIImage(data: data)
-            }
+    }
+    
+    func handleRandomImageResponse(imageData : DogImage? , error : Error?){
+        
+        guard let imageUrl = URL(string: imageData?.message ?? "") else {
+            return
         }
-        task.resume()
+        
+        DogAPI.requestImageFile(url: imageUrl, completionHandler: self.handleImageFileResponce(image:error:))
     }
 
-
+    func handleImageFileResponce(image : UIImage?, error :Error?){
+        DispatchQueue.main.async {
+            self.imageView.image = image
+        }
+    }
 }
 
--- Randog/Model/DogAPI.swift
+++ Randog/Model/DogAPI.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 class DogAPI {
     enum Endpoint : String {
         case randomImageFromAllDogCollection = "https://dog.ceo/api/breeds/image/random"
@@ -15,5 +16,36 @@ class DogAPI {
             return URL(string: self.rawValue)!
         }
     }
-    
+    class func requestRandomImage(completionHandler :@escaping (DogImage?, Error?) ->Void ){
+        let randomImageEndpoint = DogAPI.Endpoint.randomImageFromAllDogCollection.url
+        
+        let task = URLSession.shared.dataTask(with: randomImageEndpoint) { (data, responce, error) in
+            guard let data = data else{
+                completionHandler(nil, error)
+                return
+            }
+
+            let decoder = JSONDecoder()
+            let imageData = try! decoder.decode(DogImage.self, from: data)
+            print(imageData)
+            completionHandler(imageData, nil)
+        }
+                    
+            
+            task.resume()
+    }
+        
+       class func requestImageFile(url : URL ,  completionHandler : @escaping (UIImage? , Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else{
+                completionHandler(nil,error)
+                return
+                
+            }
+            let downloadedImage = UIImage(data: data)
+            completionHandler(downloadedImage, nil)
+    }
+        task.resume()
 }
+}
+
